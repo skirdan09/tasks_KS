c8a06e4854d6ea7fcdbcb1a6de66ad54ee811352
София Кирдан <lol_tik77@mail.ru>
2025-07-09 16:03:05 +0500

    1

---
+++ task04_01/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task04_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину массива");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ширину массива");
+            int b = int.Parse(Console.ReadLine());
+            int[,] = new int[a, b];
+            Random random = new Random();
+            for (int i = 0; i < a; i++)
+              for (int j = 0; j <  ; j++)
+        }
+    }
+}
