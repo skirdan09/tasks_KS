65f75a51496b39c6cc21c7f0f343c601ced2ff94
София Кирдан <lol_tik77@mail.ru>
2025-07-22 12:27:35 +0500

    урок 8 з 1

---
+++ task08_01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task08_01
+{
+    using System;
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public Person(string name, int age)
+        {
+            Name = name; 
+            Age = age;   
+        }
+        // Метод: выводит информацию о человеке в консоль
+        public void Describe()
+        {
+            Console.WriteLine("Имя: " + Name);
+            Console.WriteLine("Возраст: " + Age);
+        }
+    }
+}
