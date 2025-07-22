f676678b7af26aac9cd00bb85d1efbb343ce9823
София Кирдан <lol_tik77@mail.ru>
2025-07-22 12:21:14 +0500

    урок 6 з 2

---
+++ task06_02/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task06_02
+{
+    using System;
+
+    class Program
+    {
+        static int FindFirstCommaIndex(string input)
+        {
+            return input.IndexOf(',');
+        }
+        static void Main()
+        {
+            Console.WriteLine("Введите строку:");
+            string userInput = Console.ReadLine();
+
+            int index = FindFirstCommaIndex(userInput);
+
+            Console.WriteLine("Индекс первой запятой: " + index);
+        }
+    }
+}
