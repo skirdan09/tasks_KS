c0c9624e78ea0c9efbcdccdcfde673614e106b39
София Кирдан <lol_tik77@mail.ru>
2025-07-22 12:15:21 +0500

    урок 6 з 1

---
+++ task_06_01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task_06_01
+{
+    using System;
+
+    class Program
+    {
+
+        static void Print(params string[] words)
+        {
+            Console.WriteLine(string.Join(" ", words));
+        }
+
+        static void Main()
+        {
+            Print("Привет", "пока");
+            
+        }
+    }
+
+
+}
