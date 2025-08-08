d47d50af53349595d5c6e91160e68d65a7524661
София Кирдан <lol_tik77@mail.ru>
2025-08-08 15:58:50 +0500

    -

---
+++ 13_2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _13_2
+{
+    internal class Program
+    {
+        public static bool OpenRead { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string path = "C:\\NewFile/file.txt";
+            Console.WriteLine("Введите текст");
+            string a;
+            a = Console.ReadLine();
+            Console.WriteLine("Напишите quit,чтобы сохранить текст в файл");
+            FileInfo file = new FileInfo(path);
+            if (OpenRead)
+      
+        }
+    }
+}
